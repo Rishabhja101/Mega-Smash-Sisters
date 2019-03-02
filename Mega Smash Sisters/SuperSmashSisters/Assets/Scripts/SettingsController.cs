d2137a69811de539878e437fcfcc551e794646b3
Rishabh Jain <rishabh101.99@gmail.com>
2019-03-02 15:44:58 -0800

    updeates folders and title menu

---
+++ Mega Smash Sisters/SuperSmashSisters/Assets/Scripts/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void setBackgroundVolume(double volume)
+    {
+        Global.backgroundMusicVolume = volume;
+    }
+}

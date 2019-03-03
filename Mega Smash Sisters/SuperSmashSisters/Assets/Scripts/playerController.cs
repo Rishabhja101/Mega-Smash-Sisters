07e229d63abaeb00914a2a592912a7c472a04081
Kyle Mumma <kyle.mumma@live.com>
2019-03-03 00:41:29 -0800

    s

--- Mega Smash Sisters/SuperSmashSisters/Assets/Scripts/playerController.cs
+++ Mega Smash Sisters/SuperSmashSisters/Assets/Scripts/playerController.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,188 +19,9 @@ public class playerController : MonoBehaviour
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
+
     public GameObject fireballprefab;
     public GameObject shooterLoc;
-
-    private bool isOnPlatform = false;
-
-    public int maxJumps;
-    private int remainingJumps;
-
-    private float nextTimeToCheckIfGrounded = 0f;
-
-    private GameObject currentPlatform;
-
-    private Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        remainingJumps = maxJumps;
-        animator = GetComponent<Animator>();
-
-        if (isPlayerOne)
-        {
-            controls = new Dictionary<string, KeyCode>()
-            {
-                {"jump", KeyCode.W},
-                {"right", KeyCode.D},
-                {"left", KeyCode.A},
-                {"drop", KeyCode.S},
-                {"basic attack", KeyCode.LeftShift},
-                {"power attack", KeyCode.Space}
-
-            };
-        } else
-        {
-            controls = new Dictionary<string, KeyCode>()
-            {
-                {"jump", KeyCode.UpArrow},
-                {"right", KeyCode.RightArrow},
-                {"left", KeyCode.LeftArrow},
-                {"drop", KeyCode.DownArrow},
-                {"basic attack", KeyCode.Mouse0},
-                {"power attack", KeyCode.Mouse1}
-            };
-        }
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //basic movement
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
-        rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
-
-        //flip character
-        if (!facingRight && horizontalMovement > 0 && !xScaleFreeze)
-        {
-            flip();
-        }
-        else if (facingRight && horizontalMovement < 0 && !xScaleFreeze)
-        {
-            flip();
-        }
-    }
-
-    void Update()
-    {
-        //move
-        if (Input.GetKeyDown(controls["left"]))
-        {
-            horizontalMovement += -1;
-        }
-        if (Input.GetKeyDown(controls["right"]))
-        {
-            horizontalMovement += 1;
-        }
-        if (Input.GetKeyUp(controls["left"]))
-        {
-            horizontalMovement -= -1;
-        }
-        if (Input.GetKeyUp(controls["right"]))
-        {
-            horizontalMovement -= 1;
-        }
-
-        //jump
-        if (Input.GetKeyDown(controls["jump"]) && remainingJumps > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            remainingJumps--;
-            nextTimeToCheckIfGrounded = Time.time + .15f;
-        }
-
-        if ((isGrounded || isOnPlatform) && remainingJumps < maxJumps && Time.time >= nextTimeToCheckIfGrounded &&
-            rb.velocity.y == 0)
-        {
-            remainingJumps = maxJumps;
-        }
-
-        //drop
-        if (Input.GetKeyDown(controls["drop"]) && isOnPlatform)
-        {
-            platformDrop();
-            Invoke("platformDrop", 0.4f);
-        }
-
-        //attack
-        if (Input.GetKeyDown(controls["power attack"]))
-        {
-            animator.SetTrigger("powerAttack");
-            Invoke("shootBall", 0.4f);
-            freezeXScale();
-            Invoke("freezeXScale", 0.5f);
-        }
-    }
-
-    void shootBall()
-    {
-        GameObject fireball = Instantiate(fireballprefab);
-        fireball.transform.position = new Vector2(shooterLoc.transform.transform.position.x + 5 * transform.localScale.x - 0.8f, shooterLoc.transform.position.y);
-        fireball.GetComponent<FireballController>().direction = transform.localScale.x;
-        fireball.GetComponent<FireballController>().col = gameObject.name;
-    }
-
-    void flip()
-    {
-        facingRight = !facingRight;
-        Vector3 scaler = transform.localScale;
-        scaler.x *= -1;
-        transform.localScale = scaler;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 10) //platform layer
-        {
-            isOnPlatform = true;
-            currentPlatform = collision.gameObject;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 10) //platform layer
-        {
-            isOnPlatform = false;
-        }
-    }
-
-    void platformDrop()
-    {
-        currentPlatform.GetComponent<BoxCollider2D>().enabled = !currentPlatform.GetComponent<BoxCollider2D>().enabled;
-    }
-
-    void freezeXScale()
-    {
-        xScaleFreeze = !xScaleFreeze;
-    }
-}
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class playerController : MonoBehaviour
-{
-    public bool isPlayerOne;
-    public float speed;
-    public float jumpForce;
-    public Dictionary<string, KeyCode> controls;
-
-    private float horizontalMovement = 0;
-    private Rigidbody2D rb;
-
-    private bool facingRight = true;
-    private bool xScaleFreeze = false;
-
-    private bool isGrounded;
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask whatIsGround;
 
     private bool isOnPlatform = false;
 
@@ -320,6 +140,7 @@ public class playerController : MonoBehaviour
         if (Input.GetKeyDown(controls["power attack"]))
         {
             animator.SetTrigger("powerAttack");
+            Invoke("shootBall", 0.4f);
             freezeXScale();
             Invoke("freezeXScale", 0.5f);
         }
@@ -329,6 +150,14 @@ public class playerController : MonoBehaviour
         {
             animator.SetTrigger("taunt");
         }
+    }
+
+    void shootBall()
+    {
+        GameObject fireball = Instantiate(fireballprefab);
+        fireball.transform.position = new Vector2(shooterLoc.transform.transform.position.x + 5 * transform.localScale.x - 0.8f, shooterLoc.transform.position.y);
+        fireball.GetComponent<FireballController>().direction = transform.localScale.x;
+        fireball.GetComponent<FireballController>().col = gameObject.name;
     }
 
     void flip()
@@ -366,4 +195,3 @@ public class playerController : MonoBehaviour
         xScaleFreeze = !xScaleFreeze;
     }
 }
->>>>>>> 4db913735504bbf1360c5a470e33ca3de25a2ca6

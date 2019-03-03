eb45035369c058a21a9d867b369ca0bacfe2d775
cdlarge77 <48164049+cdlarge77@users.noreply.github.com>
2019-03-03 07:04:48 -0800

    uuu

---
+++ Mega Smash Sisters/SuperSmashSisters/Assets/Scripts/Animation Event Audio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventAudio : MonoBehaviour
+{
+
+    public AudioClip sound;
+    public AudioSource audioS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void PlaySound()
+    {
+        audioS.PlayOneShot(sound);
+    }
+}

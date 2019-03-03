4e580ab627c99b7efbb548108ef216068ece17d9
Rishabh Jain <rishabh101.99@gmail.com>
2019-03-02 18:32:31 -0800

    did work on character selection

--- Mega Smash Sisters/SuperSmashSisters/Assets/Scripts/CharacterSelectorController.cs
+++ Mega Smash Sisters/SuperSmashSisters/Assets/Scripts/CharacterSelectorController.cs
@@ -6,10 +6,14 @@ public class CharacterSelectorController : MonoBehaviour
 {
     private int players;
 
+    [SerializeField]
+    private GameObject playerBoxPrefab;
+
 	// Use this for initialization
 	void Start ()
     {
         players = 2;
+        displayPlayerBoxes();
 	}
 	
 	// Update is called once per frame
@@ -17,4 +21,20 @@ public class CharacterSelectorController : MonoBehaviour
     {
 		
 	}
+
+    private void displayPlayerBoxes()
+    {
+        float width = (1020 - 20 * (players + 1)) / players;
+        float first = width / 2 + 20;
+        GameObject[] playerBoxes = new GameObject[players];
+        for (int i = 0; i < players; i++)
+        {
+            float pos = first + i * (width + 20);
+            playerBoxes[i] = Instantiate(playerBoxPrefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+            //     playerBoxes[i].GetComponent<RectTransform>().sizeDelta.Set(200, 50);
+            playerBoxes[i].transform.localScale = new Vector3(1, 1, 1);
+            playerBoxes[i].transform.parent = GameObject.Find("/Canvas").transform;
+            print("hi");
+        }
+    }
 }
